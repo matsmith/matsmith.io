cc0e6477d083851241068f62baa63a22800c3d53
Mathew Smith <msmit205@its.jnj.com>
2014-11-03 10:55:38 -0500

    Fixed nested document ready calls in JS.
    There was a really bad js implementation...obviously the person
    who wrote the original didn't know anything about javascript.

--- js/script.js
+++ js/script.js
@@ -1,40 +1,33 @@
-(function($) {
-    $(function() {
-        var videobackground = new $.backgroundVideo($('.background-day-to-day'), {
-            "align" : "centerXY",
-            "path" : "video/",
-            "width": 854,
-            "height": 480,
-            "filename" : "preview",
-            "types" : ["mp4", "ogg", "webm"]
-        });
-
-        // Sections height & scrolling
-        $(window).resize(function() {
-            var sH = $(window).height();
-            $('section').css('height', (sH + 'px'));
-        });        
-
-        // Parallax
-        $('section').each(function() {
-            $(this).parallax('50%', 0.3, true);
-        });
-
-        $(window).resize().scroll();
-
-        // Scroll to next section
-        $('.control-btn').on('click', function() {
-            $.scrollTo($(this).closest('header, section').next(), {
-                axis : 'y',
-                duration : 500
-            });
-            return false;
-        });
-
+$(function() {
+    var videobackground = new $.backgroundVideo($('.background-day-to-day'), {
+        "align" : "centerXY",
+        "path" : "video/",
+        "width": 854,
+        "height": 480,
+        "filename" : "preview",
+        "types" : ["mp4", "ogg", "webm"]
     });
 
-    $(window).load(function() {
-        $('html').addClass('loaded');
-        $(window).resize().scroll();
+    // Sections height & scrolling
+    $(window).resize(function() {
+        var sH = $(window).height();
+        $('header, section').css('height', (sH + 'px'));
     });
-})(jQuery);
+
+    // Parallax
+    $('header, section').each(function() {
+        $(this).parallax('50%', 0.3, true);
+    });
+
+    // Scroll to next section
+    $('.control-btn').on('click', function() {
+        $.scrollTo($(this).closest('header, section').next(), {
+            axis : 'y',
+            duration : 500
+        });
+        return false;
+    });
+
+
+    $('html').addClass('loaded');
+});
